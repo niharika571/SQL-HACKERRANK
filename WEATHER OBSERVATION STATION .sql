9b3aad57efe2980bebeb1737a80ec67be7f45643
niharika571 <71439752+niharika571@users.noreply.github.com>
2022-04-01 10:54:41 +0530

    Update WEATHER OBSERVATION STATION .sql

--- WEATHER OBSERVATION STATION .sql
+++ WEATHER OBSERVATION STATION .sql
@@ -2,14 +2,16 @@
 Query a list of CITY and STATE from the STATION table.
 
 The STATION table is described as follows:
-
-
-
-
-
-
-
-
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
 
 
 SELECT CITY,STATE FROM STATION
